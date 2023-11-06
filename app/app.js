33a9d34b137be41e861c43085ce852cd4ffb6ee5
Nariman <onlnaovnar@gmail.com>
2023-11-06 22:48:05 +0600

    first commit

---
+++ app/app.js
@@ -0,0 +1,44 @@
+
+require('dotenv').config({})
+// Express
+const express = require('express')
+const app = express()
+
+//DB
+const { Pool } = require('pg')
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+
+// Router
+const router = express.Router()
+
+// Главная
+router.get('/', (_req, res) => {
+  res.status(200).json({
+    message: 'Hello World',
+  })
+})
+
+// Обработка всего остального
+router.get('/*', (_req, res) => {
+  res.status(400).json({
+    error: 'Запрос не может быть обработан, маршрут не найден'
+  })
+})
+
+// Routes
+app.use('/', router)
+
+app.listen(3000, () => {
+  console.log('Сервер запущен')
+})
